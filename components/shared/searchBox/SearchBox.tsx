f2e050e94c20d21c94785f98441b823dbcc3c563
stroich <stroichmwork@gmail.com>
2023-10-18 17:56:43 +0400

    feat: add search

--- components/shared/searchBox/SearchBox.tsx
+++ components/shared/searchBox/SearchBox.tsx
@@ -1,15 +1,33 @@
 import Image from 'next/image';
 
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+
 import search from '../../../public/assets/img/search.svg';
 
 const SearchBox = () => {
+  const router = useRouter();
+  const [searchTerm, setSearchTerm] = useState('');
+  const handleInputChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter' && searchTerm) {
+      router.push(`/search?t=${searchTerm}`);
+    }
+  };
+
   return (
     <div className="flex items-center border rounded-full bg-gray-100 p-2 shadow-md w-[160px] sm:w-auto">
       <Image src={search} alt="Search" className="w-[17px] h-[17px] mr-2" />
       <input
+        value={searchTerm}
         type="text"
         placeholder="Искать рецепт"
         className="outline-none border-none focus:ring-0 w-full bg-gray-100 sm:ml-5"
+        onChange={handleInputChange}
+        onKeyPress={handleKeyPress}
       />
     </div>
   );
---
+++ pages/search.tsx
@@ -0,0 +1,60 @@
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+
+import RecipeCardOnMain from '../components/entitites/RecipeCardOnMain';
+import HomeLayout from '../components/shared/layouts/homeLayout';
+import { getRandomElementsFromArray } from '../components/shared/RandomElements/RamdomElements';
+import { Folders } from '../interfaces/interfaces';
+import postMetadata from '../service/postMetadata';
+
+export async function getStaticProps() {
+  const posts = await postMetadata(Folders.Recipes);
+  return {
+    props: {
+      posts,
+    },
+    revalidate: 1,
+  };
+}
+
+const SearchPage = ({ posts }) => {
+  const router = useRouter();
+  const { t } = router.query;
+
+  const [filteredPosts, setFilteredPosts] = useState([]);
+
+  useEffect(() => {
+    const searchPosts = async () => {
+      if (t) {
+        const regexT = new RegExp(`${t}`, 'igu');
+        const regexTMinusOne = new RegExp(`${t.slice(0, -1)}`, 'igu');
+
+        let filtered = posts.filter((post) => {
+          return regexT.test(post.title) || regexTMinusOne.test(post.title);
+        });
+        if (filtered.length === 0) {
+          filtered = posts.filter((post) => {
+            return regexT.test(post.subtitle) || regexTMinusOne.test(post.subtitle);
+          });
+        }
+        setFilteredPosts(filtered);
+      }
+    };
+    searchPosts();
+  }, [t]);
+
+  return (
+    <HomeLayout title={'Блог'}>
+      <div className="md:px-16">
+        <h3 className={'my-10'}>Результаты поиска по запросу: {t}</h3>
+        <div className="grid 2xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-3 grid-cols-1 gap-2">
+          {filteredPosts.map((post) => (
+            <RecipeCardOnMain key={post.slug} recipe={post} />
+          ))}
+        </div>
+      </div>
+    </HomeLayout>
+  );
+};
+
+export default SearchPage;
